f4ccbcd8cd4e6b043585091778a4cc154b1b12c4
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-07-25 17:24:15 +0300

    terraform fmt

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "network_interface" {
     name                          = "testconfiguration1"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = var.public_ip_id
+    public_ip_address_id          = var.public_ip_id
   }
 }
 
